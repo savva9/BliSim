deef1a491028e588d324fc95805580f04cf1afd7
savva_9 <148879501+savva9@users.noreply.github.com>
2025-01-26 23:26:46 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -90,7 +90,7 @@ function updateArrowPosition() {
     let accumulatedWidth = 0;
 
     document.querySelectorAll(".segment").forEach(segment => {
-        const segmentWidth = segment.offsetWidth + 5;
+        const segmentWidth = segment.offsetWidth + 1.5;
 
         // Проверим, находится ли стрелка в пределах текущего сегмента
         if (arrowPosition >= accumulatedWidth && arrowPosition <= accumulatedWidth + segmentWidth) {
